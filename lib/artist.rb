05b456e0c689bb709e9ac9810fa52f1b46a3cd50
Catherine Price <catprice444@gmail.com>
2020-05-25 15:57:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,19 +14,18 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    # @@song_count += 1
+    @@song_count +=1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     self.songs << song
     song.artist = self
-    # @@song_count +=1
+    @@song_count +=1
   end
 
   def songs
     Song.all.select {|song| song.artist == self}
-    @@song_count +=1
   end
 
   def self.song_count
