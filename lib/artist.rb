0f2ce432612f4ae0693a57b97f3986c377717205
Catherine Price <catprice444@gmail.com>
2020-05-25 14:49:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,28 @@
+# class Artist
+#   attr_accessor :name
+#
+#   @@song_count = 0
+#
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#   end
+#
+#   def songs
+#     @songs
+#   end
+#
+#   def add_song(song)
+#     @songs << song
+#     self.artist = self
+#     @@song_count += 1
+#   end
+#
+# end
+
 class Artist
-  attr_accessor :name
+
+  attr_accessor :name, :songs
 
   @@song_count = 0
 
@@ -8,14 +31,21 @@ class Artist
     @songs = []
   end
 
-  def songs
-    @songs
-  end
-
   def add_song(song)
     @songs << song
-    self.artist = self
+    song.artist = self
     @@song_count += 1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
   end
 
 end
